4d45f78d57e436c856e20fc63b3bf223ff0f82ba
shaas6 <seanhaas@live.com>
2021-06-23 14:36:36 -0400

    Script 4 compilation error fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,9 +8,7 @@
  */
 int main(void)
 {
-	char str1[];
-
-	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+	char str1[] = "\"Programming is like building a multilingual puzzle"
 
 	puts(str1);
 
