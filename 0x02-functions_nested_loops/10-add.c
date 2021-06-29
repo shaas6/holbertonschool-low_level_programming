723451f04b5edd723017ab9ec09ec48455769fd4
shaas6 <seanhaas@live.com>
2021-06-29 11:00:01 -0400

    Program 10- Simple addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - function adding two variables
+ * @a: variable used in addition problem
+ * @b: second variable used in addition problem
+ * Return: Result of the addition
+ */
+int add(int a, int  b)
+{
+	return (a + b);
+}
