227619d9e84fa16c20fa9ae8c92734ff28598b60
shaas6 <seanhaas@live.com>
2021-07-02 13:33:25 -0400

    Program 4- Print numbers except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,22 @@
 #include "holberton.h"
+
+/**
+ * print_most_numbers - print numbers 0-9 except 2 and 4
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if ((i == '2') || (i == '4'))
+		{
+			continue;
+		}
+		else
+			_putchar(i);
+	}
+	putchar('\n');
+}
