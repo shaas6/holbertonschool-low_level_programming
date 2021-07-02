19204e17123492628794ce0a676306e1f16c301c
shaas6 <seanhaas@live.com>
2021-07-02 12:35:28 -0400

    Program 3- Print 0-9 with putchar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,18 @@
 #include "holberton.h"
+
+/**
+ * print_numbers - prints 0-9 followed by a newline
+ * @x - variable being printed
+ * _putchar - command to print a variable
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
