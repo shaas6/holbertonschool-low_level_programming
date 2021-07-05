b912b0deac6f3ee5777fd95b0bc0ee5977d81bcb
shaas6 <seanhaas@live.com>
2021-07-05 14:39:11 -0400

    Fixing comment description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - set pointer as parameter and update value to 98
- * @n: variable being pointed to
+ * @i: variable being pointed to
  * Return: 0
  */
 void reset_to_98(int *i)
