1d397163a98fc2d83cd8c089586a532824d91758
shaas6 <seanhaas@live.com>
2021-07-05 16:05:06 -0400

    Program 3- Puts, print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _puts - check the code for Holberton School students.
+ * @str: string being printed 
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
