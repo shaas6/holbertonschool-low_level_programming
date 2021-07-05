9f6b7110eb6d0304ee527ac2f0e24646041367c4
shaas6 <seanhaas@live.com>
2021-07-05 14:36:45 -0400

    First push for pointers project with Program 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - set pointer as parameter and update value to 98
+ * @n: variable being pointed to
+ * Return: 0
+ */
+void reset_to_98(int *i)
+{
+	*i = 98;
+}
