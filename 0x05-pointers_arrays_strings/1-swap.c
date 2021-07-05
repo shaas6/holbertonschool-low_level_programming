ae83a39c46f4b9c5e6ef3af8102c02dba4a05e86
shaas6 <seanhaas@live.com>
2021-07-05 15:17:51 -0400

    Program 1 - Swap two ints with a pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ * @a: variable one
+ * @b: variable two
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
