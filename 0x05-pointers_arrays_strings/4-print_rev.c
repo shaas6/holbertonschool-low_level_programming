638256f91d978d6dfe3e774022ffb7d1de7795f1
shaas6 <seanhaas@live.com>
2021-07-06 11:52:45 -0400

    Program 4 - reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_rev - reverses string
+ * @s: string itself
+ * Return: string reversed
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlen(s);
+	i--;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	putchar('\n');
+}
