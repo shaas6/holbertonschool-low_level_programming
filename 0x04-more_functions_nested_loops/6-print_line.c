7122930c11de29d8c5b4b5c30e4001ae25238f3e
shaas6 <seanhaas@live.com>
2021-07-07 13:38:02 -0400

    Program 6 - print a line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,18 @@
 #include "holberton.h"
+
+/**
+ * print_line - print '_' the number of times specified
+ * @n: how many _ to print
+ * Return: 0
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		for (; n > 0; n--)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
