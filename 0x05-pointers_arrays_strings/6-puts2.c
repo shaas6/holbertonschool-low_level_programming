97038da1b1176fc002256bc9624226c8817f10b5
shaas6 <seanhaas@live.com>
2021-07-06 20:28:51 -0400

    Program 6- print every other letter

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * puts2 - reverses string
+ * @str: string itself
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i = 0
+
+	for (i = 0; i < _strlen(str); i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: string being checked
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int num;
+
+	num = 0;
+
+	while (*s != '\0')
+	{
+		num++;
+		s++;
+	}
+	return (num);
+}
