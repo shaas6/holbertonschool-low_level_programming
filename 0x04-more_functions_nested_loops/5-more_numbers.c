c7648ba326c68eea5dc8f97162b7b2ea0e1b345d
shaas6 <seanhaas@live.com>
2021-07-07 11:24:31 -0400

    Program 5- Print a string 10 times with putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,24 @@
 #include "holberton.h"
+
+/**
+ * more_numbers - print a long number 10 times
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i, b;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+			{
+				_putchar('1');
+			}
+			_putchar(b % 10 + '0');
+		}
+	_putchar('\n');
+	}
+}
