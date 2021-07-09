dc8cc51693a71629e9daca5b91504656a19dde9c
shaas6 <seanhaas@live.com>
2021-07-08 23:02:52 -0400

    Program 2 add

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - concatenates two strings by n bytes at most
+ * @src: string to be copied
+ * @dest: destination string
+ * @n: number of characters to append
+ * Return: destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n && src[i]; i++;
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
