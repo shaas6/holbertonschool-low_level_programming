ebcbf494a85645f8ba72ccb39c342b0d9f2b8bfd
shaas6 <seanhaas@live.com>
2021-07-08 23:05:41 -0400

    Program 2 syntax fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n && src[i]; i++;
+	for (i = 0; i < n && src[i]; i++;)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
 		dest[i] = '\0';
