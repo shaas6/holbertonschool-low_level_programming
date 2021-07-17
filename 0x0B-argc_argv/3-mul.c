16b7fd6ce99cb408540649a7d4c4c54a1880b873
shaas6 <seanhaas@live.com>
2021-07-16 21:48:23 -0400

    Program 3- multiply. add

---
+++ 0x0B-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplication of two arguments with atoi
+ * @argc: array of number of arguments to be called
+ * @argv: the strings to be stored in the array argv
+ * Return: Error returns 1, good math returns 0
+ */
+int main(int argc, char *argc[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
