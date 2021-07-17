057c8c731466f0d212bed2b0975eb0763aa72dee
shaas6 <seanhaas@live.com>
2021-07-16 21:34:29 -0400

    Program 2 add

---
+++ 0x0B-argc_argv/2.args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints any arguments entered with the program exe
+ * @argv: points to the different arguments
+ * @argc: count of the number of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int input;
+
+	for (input = 0; x < argc; input++)
+		printf("%s\n", argv[input]);
+	return (0);
+}
