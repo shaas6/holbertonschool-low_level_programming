350ae49081dde3cd58cc6c27b562afd23f537ab6
shaas6 <seanhaas@live.com>
2021-07-22 06:57:13 -0700

    Task 2 add - string duplication

---
+++ 0x0C-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strdup - makes copy of string and returns pointer to it
+ * @str: origin string
+ * Return: Null or point to the new string
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *new_str;
+	int i;
+	int length = 0;
+
+	if (!str)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		length++;
+
+	new_str = malloc(sizeof(char) * (length + 1));
+
+	if (!new_str)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		new_str[i] = str[i];
+
+	new_str[length] = '\0';
+
+	return (new_str);
+}
