c98ebdcca71686cedbdfaa842ec5a63521c5bc52
shaas6 <seanhaas@live.com>
2021-07-26 19:06:06 -0700

    First add for readme and first task

---
+++ 0x0F-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - info de doge
+ * @name: name of doge
+ * @age: age of doge
+ * @owner: whose doge is it
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
