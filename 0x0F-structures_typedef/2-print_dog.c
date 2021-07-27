3fca12d62f7b33cd4a103c5867793b6e3dcbcf92
shaas6 <seanhaas@live.com>
2021-07-26 20:13:38 -0700

    Task 2 add

---
+++ 0x0F-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - print doge
+ * @d: ptr to dog struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %f\n", d->age);
+		if (d->owner)
+			printf("Owner: %s\n" d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
