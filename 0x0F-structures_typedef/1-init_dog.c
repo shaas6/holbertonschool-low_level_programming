5fe238295cf9a2b6b76e46d9dd92c5eaec3db11a
shaas6 <seanhaas@live.com>
2021-07-26 19:17:06 -0700

    Added prototypes to header and Task 1 add

---
+++ 0x0F-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * init_dog - initialize variable of struct dog
+ * @d: ptr
+ * @name: doge's name
+ * @age: just a number
+ * @owner: doge's owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
