48a595211bb2ecf23a2a5de9bcd88ff1d3dc0e7f
shaas6 <seanhaas@live.com>
2021-07-27 10:36:26 -0700

    First add for header, readme and Task 0 - print a name

---
+++ 0x10-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: string containing name
+ * @f: the function being called
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		(*f)(name);
+}
