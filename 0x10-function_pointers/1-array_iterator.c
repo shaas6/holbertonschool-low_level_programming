c4f382cfe825256bf72b6f4c8034f2a5bf6ed272
shaas6 <seanhaas@live.com>
2021-07-27 19:20:10 -0700

    Task 1 add

---
+++ 0x10-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - executes function given as paramater
+ * @array: numbers
+ * @size: number of elements in the array
+ * @action: which function
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int x;
+	if (array == NULL || action == NULL)
+		return;
+	for (x = 0; x < size; x++)
+		(*action)(array[x]);
+}
