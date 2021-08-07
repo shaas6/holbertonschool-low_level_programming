ce9893dde8cab016982e612dc6102548b32a332c
shaas6 <seanhaas@live.com>
2021-08-06 18:55:03 -0700

    First add of files for singly linked lists project

---
+++ 0x13-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the elements of a linked list
+ * @h: head
+ * Return: node count
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *ptr = h;
+	size_t count = 0;
+
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+			printf("[%u] %p\n", ptr->len, ptr->str);
+		else
+			printf("[%u] %s\n", ptr->len, ptr->str);
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
