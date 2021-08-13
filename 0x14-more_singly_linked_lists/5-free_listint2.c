730709fb7d5f9f2ac7e59ec4645b053dff4f0522
shaas6 <seanhaas@live.com>
2021-08-13 06:53:16 -0700

    Task 5 add

---
+++ 0x14-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free all the nodes, head is NULL
+ * @head: ptr to first node
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *help;
+
+	while (head != NULL)
+	{
+		help = *head;
+		*head = *head->next;
+		free(help);
+	}
+	*head = NULL;
+}
