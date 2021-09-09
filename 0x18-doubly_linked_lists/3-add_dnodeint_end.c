147c55703a8c8e837279c98c2f4f3edfd864ab0c
shaas6 <seanhaas@live.com>
2021-09-08 23:09:15 -0400

    Forgot an =

--- 0x18-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new, *temp;
 
 	new = malloc(sizeof(dlistint_t));
-	if (new = NULL)
+	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
