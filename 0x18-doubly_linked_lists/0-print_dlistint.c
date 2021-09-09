3a0a25a35b66593acb2e55b12a756bd6db2d557a
shaas6 <seanhaas@live.com>
2021-09-08 22:14:02 -0400

    Task 0 edit

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
- *
- *
+ * print_dlistint - print a list
+ * @h: head of the list
+ * Return: number of nodes
  */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *now = NULL;
+	unsigned int node = 0;
+
+	if (h)
+	{
+		now = h;
+		while (now)
+		{
+			printf("%d\n", now->n);
+			now = now->next;
+			node++;
+		}
+	}
+	return (node);
+}
